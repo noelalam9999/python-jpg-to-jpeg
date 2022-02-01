15911231237a1a9f17020795336c855b6a2c21a9
unknown <noel.alam9999@gmail.com>
2022-02-01 17:40:45 +0600

    any path

--- convert.py
+++ convert.py
@@ -1,10 +1,13 @@
 import os
 from PIL import Image
-
-for filename in os.listdir('D:/furniture shop/Furniture-Ecommerce/public/assets/images/product/bathroom/doors/1'):
-    i = 1
+path = os.getcwd()
+print(path)
+i = 1
+for filename in os.listdir(path):
+    print(filename)
+    
     if filename.endswith('.jpg'):
-        with Image.open(os.path.join('D:/furniture shop/Furniture-Ecommerce/public/assets/images/product/bathroom/doors/1/', filename)) as f:
-            file = 'D:/furniture shop/Furniture-Ecommerce/public/assets/images/product/bathroom/doors/1/'+str(i)+'.jpeg'
+        with Image.open(os.path.join(path, filename)) as f:
+            file = path+"\\"+str(i)+'.jpeg'
             f.save(file)
             i=i+1
